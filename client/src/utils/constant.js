8032499e45067b8a8951ef9bb922b3d80ff37278
Andrew <andrewlin1368@gmail.com>
2024-02-16 15:57:38 -0500

    routes

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -12,45 +12,69 @@ export const USER_CREDENTIALS = "user_credentials";
 
 // backend base url for development and production
 export const BACKEND_BASE_URL = import.meta.env.DEV
-	? import.meta.env.VITE_DEVELOPMENT_BACKEND_BASE_URL
-	: import.meta.env.VITE_PRODUCTION_BACKEND_BASE_URL;
+  ? import.meta.env.VITE_DEVELOPMENT_BACKEND_BASE_URL
+  : import.meta.env.VITE_PRODUCTION_BACKEND_BASE_URL;
 
 /* PRODUCT ROUTES */
 
 export function getAllProductsRoute() {
-	return "/products";
+  return "/products";
 }
 
 export function getProductByIdRoute(id) {
-	return `/products/product/${id}`;
+  return `/products/product/${id}`;
 }
 
 export function getCreateProductRoute() {
-	return "/products/create/";
+  return "/products/create/";
 }
 
 export function getUpdateProductRoute(id) {
-	return `/products/update/${id}`;
+  return `/products/update/${id}`;
 }
 
 export function getRemoveProductRoute(id) {
-	return `/products/delete/${id}`;
+  return `/products/delete/${id}`;
 }
 
 /* USER ROUTES */
 
 export function getAllUsersRoute() {
-	return "/user/";
+  return "/user/";
 }
 
 export function getUserRegisterRoute() {
-	return "/user/register/";
+  return "/user/register/";
 }
 
 export function getUserLoginRoute() {
-	return "/user/login";
+  return "/user/login";
 }
 
 export function getUserProfileRoute() {
-	return "/user/profile";
+  return "/user/profile";
+}
+
+export function getInCartRoute() {
+  return "/cart/incart";
+}
+
+export function getCheckoutOrderRoute() {
+  return "/cart/checkout";
+}
+
+export function getCancelOrderRotue() {
+  return "/cart/cancel";
+}
+
+export function getRemoveItemRoute() {
+  return "/cart/delete";
+}
+
+export function getIncreaseCountRoute() {
+  return "/cart/updateUp";
+}
+
+export function getDecreaseCountRoute() {
+  return "/cart/updateDown";
 }
